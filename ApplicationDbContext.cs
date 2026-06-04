5508621676b29c68c4a1398cd46e00e023c30702
Zku11 <lucasferr87@yahoo.com.ar>
2026-06-03 21:57:08 -0300

    first commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerInAspNet.Entities;
+
+namespace TaskManagerInAspNet
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<UserTask> UserTasks { get; set; }
+    }
+}
---
+++ Entities/UserTask.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerInAspNet.Entities
+{
+    public class UserTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
