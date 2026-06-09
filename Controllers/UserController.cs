eb37d666757b547c4dc37efe8e2e235965f78e75
Zku11 <lucasferr87@yahoo.com.ar>
2026-06-08 21:48:32 -0300

    User Login. Starting with external login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using TaskManagerInAspNet.Models;
 
 namespace TaskManagerInAspNet.Controllers
@@ -48,6 +50,103 @@ namespace TaskManagerInAspNet.Controllers
             {
                 return View(viewModel);
             }
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string? message = null)
+        {
+            if (message is not null)
+            {
+                ViewData["message"] = message;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var operation = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (operation.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Nombre de usuario o password incorrecto");
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ChallengeResult ExternalLogin(string provider, string? returnUrl = null)
+        {
+            var redirectUrl = Url.Action("RegisterExternalUser", values: new { returnUrl });
+            var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, returnUrl);
+            return new ChallengeResult(provider, properties);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> RegisterExternalUser(string? returnUrl = null, string? remoteError = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            var message = "";
+            if(remoteError is not null)
+            {
+                message = $"Error del proveedor externo: {remoteError}";
+                return RedirectToAction("Login", routeValues: new {message});
+            }
+            var info = await signInManager.GetExternalLoginInfoAsync();
+            if (info is null)
+            {
+                message = "Error al cargar la data del inicio de sesión externo";
+                return RedirectToAction("Login", routeValues: new { message });
+            }
+            var externalLoginResult = await signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: true, bypassTwoFactor: true);
+            if (externalLoginResult.Succeeded)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            string? email = null;
+            if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
+            {
+                email = info.Principal.FindFirstValue(ClaimTypes.Email);
+            }
+            else
+            {
+                message = "Error al obtener el email del usuario del proveedor";
+                return RedirectToAction("login", routeValues: new { message });
+            }
+            var user = new IdentityUser(){ Email = email, UserName = email};
+            var userCreationResult = await userManager.CreateAsync(user);
+            if (!userCreationResult.Succeeded)
+            {
+                message = userCreationResult.Errors.First().Description;
+                return RedirectToAction("Login", routeValues: new { message });
+            }
+            var userLoginResult = await userManager.AddLoginAsync(user, info);
+            if (userLoginResult.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: true, info.LoginProvider);
+                return LocalRedirect(returnUrl);
+            }
+            message = "Ha ocurrido un error al agregar el login";
+            return RedirectToAction("Login", routeValues: new { message });
         }
     }
 }
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerInAspNet.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Debe completar el campo: {0}")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Debe completar el campo: {0}")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Mantener la sesión iniciada")]
+        public bool RememberMe { get; set; }
+    }
+}
