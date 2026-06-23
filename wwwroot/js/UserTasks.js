02bff4739c7afa21159fec5e84fbcba11ef01688
Zku11 <lucasferr87@yahoo.com.ar>
2026-06-22 22:10:30 -0300

    Added new task frontend function

---
+++ wwwroot/js/UserTasks.js
@@ -0,0 +1,14 @@
+﻿function addNewTask() {
+    taskListViewModel.userTasks.push(new taskElementViewModel({ id: 0, title: '' }));
+    $("[name=task-title").last().focus();
+}
+
+function manageFocusOutTaskTitle(userTaskP) {
+    const title = userTaskP.title();
+    if (!title || title === '') {
+        taskListViewModel.userTasks.pop();
+    }
+    else {
+        userTaskP.id(1);
+    }
+}
